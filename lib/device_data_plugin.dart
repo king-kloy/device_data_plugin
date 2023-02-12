7b0462de0b50ab16a70df4c95492e69a1fca3005
Okai Yeboah <kingkloy16@gmail.com>
2023-02-12 21:44:13 +0000

    add: mobile device data plugin

--- lib/device_data_plugin.dart
+++ lib/device_data_plugin.dart
@@ -1,3 +1,21 @@
 library device_data_plugin;
 
-class DeviceDataPlugin {}
+import 'package:device_data_plugin/src/device_data_extractor.dart';
+import 'package:device_data_plugin/src/device_physical_size_extractor.dart';
+import 'package:device_data_plugin/src/models/device_data/device_data.dart';
+import 'package:device_data_plugin/src/models/device_physical_size/device_physical_size.dart';
+
+/// Provides information about mobile device
+class DeviceDataPlugin {
+  DeviceDataPlugin();
+
+  final DevicePhysicalSizeExtractor _sizeExtractor =
+      DevicePhysicalSizeExtractor();
+  final DeviceDataExtractor _dataExtractor = DeviceDataExtractorImpl();
+
+  DevicePhysicalSize retrieveDeviceSize() =>
+      _sizeExtractor.getDevicePhysicalSize();
+
+  Future<DeviceData?> retrieveDeviceData() =>
+      _dataExtractor.extractDeviceData();
+}
